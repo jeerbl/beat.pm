8d241c404fa4eec90afc913650f7b9d3f00fe300
Jerome Brunel <jb.jeromebrunel@gmail.com>
2016-07-26 23:50:37 +0200

    Add watch to Gulp and basic index.html

--- gulpfile.js
+++ gulpfile.js
@@ -8,4 +8,14 @@ gulp.task('connect', () => {
   })
 })
 
-gulp.task('default', ['connect'])
+gulp.task('html', () => {
+  gulp.src('./src/index.html')
+    .pipe(gulp.dest('./dist/'))
+    .pipe(connect.reload())
+})
+
+gulp.task('watch', () => {
+  gulp.watch(['./src/*.html'], ['html'])
+})
+
+gulp.task('default', ['connect', 'watch', 'html'])
