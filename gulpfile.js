6053ea1782705c11a2ea03ccd2f3e9aa10807c7b
Jerome Brunel <jb.jeromebrunel@gmail.com>
2016-08-01 23:36:11 +0200

    Add inline source to inline CSS and JavaScript

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp')
 var connect = require('gulp-connect')
+var inlinesource = require('gulp-inline-source')
 
 gulp.task('connect', () => {
   connect.server({
@@ -10,12 +11,13 @@ gulp.task('connect', () => {
 
 gulp.task('html', () => {
   gulp.src('./src/index.html')
+    .pipe(inlinesource())
     .pipe(gulp.dest('./dist/'))
     .pipe(connect.reload())
 })
 
 gulp.task('watch', () => {
-  gulp.watch(['./src/*.html'], ['html'])
+  gulp.watch(['./src/*.html', './src/styles/*.css', './src/scripts/*.js'], ['html'])
 })
 
 gulp.task('default', ['connect', 'watch', 'html'])
