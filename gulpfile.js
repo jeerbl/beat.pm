e558d635be3c497a4766900400ad8b2acb26282f
Jerome Brunel <jb.jeromebrunel@gmail.com>
2016-08-07 22:56:20 +0200

    Add test task for Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
-var gulp = require('gulp')
-var connect = require('gulp-connect')
-var inlinesource = require('gulp-inline-source')
+var gulp = require('gulp');
+var connect = require('gulp-connect');
+var inlinesource = require('gulp-inline-source');
 
 gulp.task('connect', () => {
   connect.server({
@@ -13,11 +13,12 @@ gulp.task('html', () => {
   gulp.src('./src/index.html')
     .pipe(inlinesource())
     .pipe(gulp.dest('./dist/'))
-    .pipe(connect.reload())
+    .pipe(connect.reload());
 })
 
 gulp.task('watch', () => {
-  gulp.watch(['./src/*.html', './src/styles/*.css', './src/scripts/*.js'], ['html'])
+  gulp.watch(['./src/*.html', './src/styles/*.css', './src/scripts/*.js'], ['html']);
 })
 
-gulp.task('default', ['connect', 'watch', 'html'])
+gulp.task('default', ['connect', 'watch', 'html']);
+gulp.task('test', ['html']);
