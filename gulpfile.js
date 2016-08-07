2b15603cbaea762634f83284e7180b42a0a94ad6
Jerome Brunel <jb.jeromebrunel@gmail.com>
2016-08-07 23:30:27 +0200

    Fix gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -2,21 +2,21 @@ var gulp = require('gulp');
 var connect = require('gulp-connect');
 var inlinesource = require('gulp-inline-source');
 
-gulp.task('connect', () => {
+gulp.task('connect', function () {
   connect.server({
     root: 'dist',
     livereload: true
   })
 })
 
-gulp.task('html', () => {
+gulp.task('html', function () {
   gulp.src('./src/index.html')
     .pipe(inlinesource())
     .pipe(gulp.dest('./dist/'))
     .pipe(connect.reload());
 })
 
-gulp.task('watch', () => {
+gulp.task('watch', function () {
   gulp.watch(['./src/*.html', './src/styles/*.css', './src/scripts/*.js'], ['html']);
 })
 
