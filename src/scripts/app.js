20fc36a2f41ca5bed3427bf3ea3e2396d23d02cc
Jerome Brunel <jb.jeromebrunel@gmail.com>
2016-08-24 00:29:04 +0200

    Display BPM on window

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,3 +1,29 @@
-document.addEventListener('click', function(e) {
-  alert('uboi');
-});
+var SPACE_KEY_CODE = 32
+var MINUTE = 60 * 1000
+
+var previousDate
+var bpms = []
+
+function newEntry (e) {
+  if (e.keyCode === SPACE_KEY_CODE || e.type === 'click') {
+    calculateBPM(new Date())
+  }
+}
+
+function calculateBPM (date) {
+  if (previousDate == null) {
+    previousDate = date
+    return
+  }
+
+  var bpm = Math.round(MINUTE / (date - previousDate) * 10) / 10
+
+  document.getElementById('bpm').innerHTML = bpm
+
+  bpms.push(bpm)
+
+  previousDate = date
+}
+
+document.onkeydown = newEntry
+document.onclick = newEntry
