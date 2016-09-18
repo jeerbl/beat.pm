324dd5e77179cce42747528a04aa550bed37b96d
Jerome Brunel <jb.jeromebrunel@gmail.com>
2016-09-18 23:44:18 +0200

    Semistandard

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,15 +1,15 @@
 window.onload = function () {
-  var SPACE_KEY_CODE = 32
-  var ENTER_KEY_CODE = 13
-  var MINUTE = 60 * 1000
+  var SPACE_KEY_CODE = 32;
+  var ENTER_KEY_CODE = 13;
+  var MINUTE = 60 * 1000;
 
   /**
    * Elements
    */
-  var bpmDisplay = document.getElementById('bpm')
-  var moreButton = document.getElementById('more')
-  var dropdown = document.getElementById('dropdown')
-  var adjustCheckbox = document.getElementById('adjust')
+  var bpmDisplay = document.getElementById('bpm');
+  var moreButton = document.getElementById('more');
+  var dropdown = document.getElementById('dropdown');
+  var adjustCheckbox = document.getElementById('adjust');
 
   /**
    * Options
@@ -17,22 +17,22 @@ window.onload = function () {
   var options = {
     adjust: !!adjustCheckbox.checked,
     previousBpmsToTake: 3
-  }
+  };
 
-  var previousDate
-  var bpms = []
-  var dropdownOpened = false
-  var started = false
+  var previousDate;
+  var bpms = [];
+  var dropdownOpened = false;
+  var started = false;
 
   function updateOptions () {
-    options.adjust = !!adjustCheckbox.checked
+    options.adjust = !!adjustCheckbox.checked;
   }
 
   function toggleDropdown (e) {
     e.stopPropagation();
-    if (dropdownOpened) dropdown.style.maxHeight = '0'
-    else dropdown.style.maxHeight = '3rem'
-    dropdownOpened = !dropdownOpened
+    if (dropdownOpened) dropdown.style.maxHeight = '0';
+    else dropdown.style.maxHeight = '3rem';
+    dropdownOpened = !dropdownOpened;
   }
 
   function stopPropagation (e) {
@@ -41,53 +41,53 @@ window.onload = function () {
 
   function newEntry (e) {
     if (dropdownOpened) {
-      toggleDropdown(e)
-      return
+      toggleDropdown(e);
+      return;
     }
     if (e.keyCode === SPACE_KEY_CODE || e.type === 'click') {
-      started = true
-      calculateBPM(new Date())
+      started = true;
+      calculateBPM(new Date());
     }
     if (e.keyCode === ENTER_KEY_CODE) {
-      endSession()
+      endSession();
     }
   }
 
   function endSession () {
-    if (!started) return
+    if (!started) return;
 
-    started = false
-    alert('oihioioh')
+    started = false;
+    alert('oihioioh');
   }
 
   function calculateBPM (date) {
     if (previousDate == null) {
-      previousDate = date
-      return
+      previousDate = date;
+      return;
     }
 
-    var bpm = Math.round((MINUTE / (date - previousDate)) * 10) / 10
+    var bpm = Math.round((MINUTE / (date - previousDate)) * 10) / 10;
     if (options.adjust) {
-      var numberOfBpms = 1
+      var numberOfBpms = 1;
       while (numberOfBpms < 3) {
-        if (bpms[bpms.length - numberOfBpms] != null) bpm += bpms[bpms.length - numberOfBpms]
-        else break
-        numberOfBpms++
+        if (bpms[bpms.length - numberOfBpms] != null) bpm += bpms[bpms.length - numberOfBpms];
+        else break;
+        numberOfBpms++;
       }
-      bpm = Math.round((bpm / numberOfBpms) * 10) / 10
+      bpm = Math.round((bpm / numberOfBpms) * 10) / 10;
     }
 
-    bpmDisplay.innerHTML = bpm.toFixed(1)
+    bpmDisplay.innerHTML = bpm.toFixed(1);
 
-    bpms.push(bpm)
+    bpms.push(bpm);
 
-    previousDate = date
+    previousDate = date;
   }
 
-  moreButton.onclick = toggleDropdown
-  dropdown.onclick = stopPropagation
-  adjust.onchange = updateOptions
+  moreButton.onclick = toggleDropdown;
+  dropdown.onclick = stopPropagation;
+  adjust.onchange = updateOptions;
 
-  document.onkeydown = newEntry
-  document.onclick = newEntry
+  document.onkeydown = newEntry;
+  document.onclick = newEntry;
 }
